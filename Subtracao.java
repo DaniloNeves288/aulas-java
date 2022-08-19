f9e48ee5bc166cc76887d95ab41fb6e8de762cb1
DaniloNeves288 <110500366+DaniloNeves288@users.noreply.github.com>
2022-08-18 22:39:04 -0300

    criando logica de subtracao

---
+++ Subtracao.java
@@ -0,0 +1,65 @@
+class Subtracao {
+  public static void main(String args []){
+int a=5; 
+int b=5;
+int resultado;
+resultado=a-b;
+    System.out.println("Subtracao of a-b = " + resultado);
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+    
+    
+  }
+
+
+}
