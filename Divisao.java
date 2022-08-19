f63bf59ee517ae37e7bc50b006e1cbd0f6f904ee
DaniloNeves288 <110500366+DaniloNeves288@users.noreply.github.com>
2022-08-18 23:02:07 -0300

    Create Divisao.java

---
+++ Divisao.java
@@ -0,0 +1,8 @@
+public class Divisao{
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int resultado=x/y;
+    System.out.println("Divisao x/y = " + resultado );
+    }
+}
